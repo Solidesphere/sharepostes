e4a87f1e2c378e1463180975236dcc54d8f40995
Kahil Mohamed Amine <45295365+Solidesphere@users.noreply.github.com>
2024-02-28 18:34:37 +0100

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,22 @@
+<?php
+ //DB Pramas
+ define('DB_HOST' , 'localhost');
+ define('DB_USER', 'root');
+ define('DB_PASS', '12345678');
+ define('DB_NAME', 'shareposts');
+
+
+
+
+//App Root
+
+define ('APPROOT',dirname(dirname(__FILE__)));
+
+// URL Root
+
+define ('URLROOT', 'http://localhost/ShareIOT');
+// Site Name 
+define('SITENAME', 'ShareIOT');
+//App Version 
+define('APPVERSION','1.0.0');
+  
